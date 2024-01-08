5311ad925e37cddeac8965d4fde0f7c3bcfad1e1
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-01-08 12:54:09 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class MainMenu{
+    //각 메뉴리스트 생성하기
+    List<>
+    public mainMenu{
+
+    }
+
+}
+public class ListMenu{
+    //각메뉴 나열하기
+}
+
+public class Main {
+    public static void Main(String[] args){
+        MainMenu mainMenu = new mainMenu(); //mainMenu생성
+        mainMenu.ListMenu();//mainMenu ListMenu 메서드 실행
+    }
+
+}
